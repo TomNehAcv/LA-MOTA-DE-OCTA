a87005dd88b651d84d17b99f74df3aef8fc72479
pc52 <pc52@LABORATORIO16.local>
2024-04-03 20:55:53 -0300

    capo hice algo

---
+++ pr1.py
@@ -0,0 +1,10 @@
+codigo
+def materia(alumnos,aula):
+    alumnos == ''
+    aula == ''
+    print(input('¿Como te llamas?: '))
+    if alumnos == 'Octavio' and aula == '209':
+        return print('DSP')
+    else:
+        return print('mala info')
+materia('Octa','309')
